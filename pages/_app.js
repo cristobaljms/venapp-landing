a56877ccdfd5801a71b8d225f3c0211c304f730e
Luis Angel España <luan.esr21@gmail.com>
2023-01-13 10:08:00 -0400

    feat: sections added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import '../styles/globals.css'
+import { poppins } from '../config/fonts';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <style jsx global>{`
+        html {
+          font-family: ${poppins.style.fontFamily};
+        }
+    `}</style>
+    <Component {...pageProps} />
+    </>
+  ) 
 }
---
+++ sections/3/index.js
@@ -0,0 +1,60 @@
+import  styles from '../../styles/Section3.module.css';
+
+const ThirdSection = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles['inner-container']}>
+        <div className={styles['videos-container']}>
+          {videos.map( (video,index) => {
+            return (
+              <div key={index}>
+                <div className={styles.square}>
+
+                </div>
+                <p>
+                  {video.text}
+                </p>
+              </div>
+            )
+          })}
+        </div>
+
+        <div className={styles.content}>
+          <h2>
+            {strings.title}
+          </h2>
+          <p>
+            {strings.description}
+          </p>
+          <button>
+            {strings.watchAllTutorials}
+          </button>
+        </div>
+      </div>
+      
+    </div>
+  )
+}
+
+const strings = {
+  title: 'Video Tutoriales',
+  watchAllTutorials: 'Ver todos los tutoriales',
+  description: 'Conoce todo lo que puedes hacer con tu VenApp y sacale provecho.'
+}
+
+const videos = [
+  {
+    text: 'Lorem ipsum dolor sit amet',
+    image: '/section_3/smartphone.png'
+  },
+  {
+    text: 'Lorem ipsum dolor sit amet',
+    image: '/section_3/smartphone.png'
+  },
+  {
+    text: 'Lorem ipsum dolor sit amet',
+    image: '/section_3/smartphone.png'
+  }
+]
+
+export default ThirdSection;
