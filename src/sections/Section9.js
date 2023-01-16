de0c58e720b60c0cbd173ec4bcf41d64083ab6ee
Cristobal Muñoz <cristobal@nolatech.ai>
2023-01-16 07:18:12 -0600

    avances

--- src/sections/Section9.js
+++ src/sections/Section9.js
@@ -1,32 +1,44 @@
+import Image from "next/image";
 import styles from "../../styles/Section9.module.css";
 
 export const Section9 = () => {
   return (
     <div className={styles.container}>
-      <div className={styles["inner-container"]}>
-        <h2>{strings.title}</h2>
-        <h4>{strings.subttitle}</h4>
-        <p>
-          {strings.description} <br></br>
-          {strings.description2}
-        </p>
-        <div className={styles.tag}>
-          <img 
-            src="/assets/img/section9/google-play.png"
-            alt="google store tag"
-          />
-          <img 
-            src="/assets/img/section9/app-store.png"
-            alt="app store tag"
-          />
+      <div className="container">
+        <div className={styles.content}>
+          <div className={styles.textContent}>
+            <h2>{strings.title}</h2>
+            <h3>{strings.subttitle}</h3>
+            <p>
+              {strings.description} <br></br>
+              {strings.description2}
+            </p>
+            <div className={styles.tag}>
+              <Image
+                src="/assets/img/section9/google-play.png"
+                alt="google store tag"
+                width={217}
+                height={64}
+              />
+              <Image
+                src="/assets/img/section9/app-store.png"
+                alt="app store tag"
+                width={217}
+                height={64}
+              />
+            </div>
+          </div>
+          <div className={styles.phoneContent}>
+            <Image
+              src="/section_4/smartphone.png"
+              alt="ven app smartphone"
+              width={694}
+              height={1330}
+              style={{ objectFit: "contain", maxWidth: "80%" }}
+            />
+          </div>
         </div>
       </div>
-
-      <img
-        src="/section_4/smartphone.png"
-        alt="ven app smartphone"
-        className={styles.phone}
-      />
     </div>
   );
 };
