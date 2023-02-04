c756c8c5b4740ed1cc97bddb2dbf5c05c5b24d2f
leojuanz <giovannimugnor@gmail.com>
2023-02-04 10:51:32 -0400

    [UPD] justify text

--- pages/c2p.js
+++ pages/c2p.js
@@ -32,7 +32,7 @@ const C2pPage = () => {
           ¿Cómo solicito la clave temporal para hacer un pago móvil C2P?
         </h1>
         <br />
-        <p>
+        <p style={{textAlign: "justify"}}>
           Aquí te explicamos cómo conseguir la clave de tu banco para realizar
           tu recarga Pago Móvil C2P:
         </p>
@@ -45,12 +45,12 @@ const C2pPage = () => {
           src="/assets/img/C2P_TABLA_F6DX6IY.width-800.png"
         />
         <br />
-        <p>
+        <p style={{textAlign: "justify"}}>
           También podrás realizarlo en la página o aplicación móvil de tu Banco:
         </p>
         <br />
         <ul className={styles.list}>
-          <li>
+          <li style={{textAlign: "justify"}}>
             <b>Banco de Venezuela:</b> Ingresar en PuntoYABDV. Ingresa con tu
             usuario y contraseña registrado en banca en línea. El banco enviará
             un código de sms afiliado al banco con un código de autenticación,
@@ -58,7 +58,7 @@ const C2pPage = () => {
             Luego seleccionamos -Configuración-. Seleccionamos -Configurar-. Por
             último, seleccionamos el código de los 8 dígitos.
           </li>
-          <li>
+          <li style={{textAlign: "justify"}}>
             <b>Banco Bicentenario:</b> Se debe ingresar a la aplicación “Tu
             Banca Móvil” introduce el número de identificación e ingresa la
             contraseña de acceso (Misma contraseña utilizada para ingresar a
@@ -67,12 +67,12 @@ const C2pPage = () => {
             sistema te mostrara el codigo que debes suministrar al comercio para
             finalizar tu ‘Pago C2P’
           </li>
-          <li>
+          <li style={{textAlign: "justify"}}>
             <b>Banco Mercantil:</b> Llego en menos de 5 segundos. Envía un SMS
             al número 24024 con las siglas SCP. Llegará a tu dispositivo móvil
             una clave que podrás usar por las próximas 6 horas para pagar
           </li>
-          <li>
+          <li style={{textAlign: "justify"}}>
             <b>Banesco:</b> El proceso tarda aproximadamente 5 min si hay que
             descargar la aplicación y se cuenta con un internet rápido. Descarga
             el aplicativo Banesco Token desde la tienda de aplicaciones de tu
@@ -80,13 +80,13 @@ const C2pPage = () => {
             usuario de acceso a BanescOnline. Ingresa la clave de operaciones
             especiales enviada a tu teléfono o correo electrónico registrado.
           </li>
-          <li>
+          <li style={{textAlign: "justify"}}>
             <b>Banco del Caribe:</b> accede a la APP Mi Pago Bancaribe, te
             autenticas y seleccionas la opción de “Generar Clave Dinámica”. Si
             aún tienes dudas haz clic aquí para dirigirte al paso a paso de tu
             banco.
           </li>
-          <li>
+          <li style={{textAlign: "justify"}}>
             <b> Banco Exterior:</b> envía un SMS al número 278 con la palabra
             CLAVE seguida de un espacio y luego el identificador (V para
             venezolano, E para Extranjero y J para Jurídico), colocas nuevamente
@@ -96,13 +96,13 @@ const C2pPage = () => {
             Si aún tienes dudas haz clic aquí para dirigirte al paso a paso de
             tu banco.
           </li>
-          <li>
+          <li style={{textAlign: "justify"}}>
             <b>BOD:</b> envía un SMS al 263 con la palabra COMERCIO (espacio)
             Cédula de Identidad. Por ejemplo: COMERCIO 11162718 .
           </li>
         </ul>
         <br />
-        <p>
+        <p style={{textAlign: "justify"}}>
           En caso de que no ubiques la información necesaria en esta
           publicación, te invitamos a que contactes con tu entidad bancaria, ya
           que gestionan la solicitud de clave temporal de forma distinta uno de
